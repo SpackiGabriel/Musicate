f0777739dea6ffaf2baf935f3dacf6883b40410a
Gabriel Henrique Alves Spacki <97992275+SpackiGabriel@users.noreply.github.com>
2022-10-24 17:46:09 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://b75abb904531b7:daa70e6e@us-cdbr-east-06.cleardb.net/heroku_7d9b43f40f807f9?reconnect=true"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://b75abb904531b7:daa70e6e@us-cdbr-east-06.cleardb.net/heroku_7d9b43f40f807f9"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 db = SQLAlchemy(app)
