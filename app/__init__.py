395d6b8beef7eb290344975a4e8548c9e817c773
Gabriel Henrique Alves Spacki <97992275+SpackiGabriel@users.noreply.github.com>
2022-10-24 17:26:21 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,7 @@ pymysql.install_as_MySQLdb()
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-#app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://root:abrl0404@127.0.0.1/musicate"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://tftuhkeidvlfmf:0323ca8cfe512c3d74812311eb0df73f02b5376f19e23e8171c24395af38904c@ec2-44-207-126-176.compute-1.amazonaws.com:5432/d3nd1ppttdbtcq"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://b75abb904531b7:daa70e6e@us-cdbr-east-06.cleardb.net/heroku_7d9b43f40f807f9?reconnect=true"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 db = SQLAlchemy(app)
